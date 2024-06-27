e5dc75b6a4c459bbe460a2610b218fd2f7a60053
Yogesh Thambidurai <thambiduraiy@hhmi.org>
2024-06-27 11:51:36 -0400

    delete no camera dataset

--- sparkle_stats/zarr_dataset.py
+++ sparkle_stats/zarr_dataset.py
@@ -98,17 +98,3 @@ class ZarrStateOnlyDataset(ZarrDataset):
     @property
     def output_classes(self):
         return 2
-
-
-class ZarrNoCameraDataset(ZarrDataset):
-    """Zarr dataset that doesn't provide camera parameters"""
-
-    def __getitem__(self, item):
-        trace, parameters = super().__getitem__(item)
-        trace = trace[0, :].unsqueeze(0)
-        parameters = parameters[[0, 1, 5, 6]]
-        return trace, parameters
-
-    @property
-    def output_classes(self):
-        return 4
