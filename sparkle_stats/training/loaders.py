34e108d3674104e59a107811f8dd2873ca32708d
Yogesh Thambidurai <thambiduraiy@hhmi.org>
2024-07-03 17:02:12 -0400

    move zarr dataset members to device in loader

--- sparkle_stats/training/loaders.py
+++ sparkle_stats/training/loaders.py
@@ -13,6 +13,9 @@ def load_path(
     ds = dataset_type(path, means=means, std_devs=std_devs, load_all=load_all)
     ds.traces = ds.traces.to(device)
     ds.parameters = ds.parameters.to(device)
+    ds.parameter_means = ds.parameter_means.to(device)
+    ds.parameter_std_devs = ds.parameter_std_devs.to(device)
+    ds.trace_mean = ds.trace_mean.to(device)
 
     loader = DataLoader(ds, batch_size=batch_size, shuffle=True)
     return ds, loader
