50392bbea0640383f196a08d6d17064d4f1a5175
Yogesh Thambidurai <thambiduraiy@hhmi.org>
2024-07-08 16:52:45 -0400

    add fully connected model

--- sparkle_stats/models/__init__.py
+++ sparkle_stats/models/__init__.py
@@ -1,7 +1,9 @@
 from .resnet1d import ResNet1D
+from .simple_fully_connected import SimpleFullyConnected
 from .vgg1d import Vgg1D
 
 __all__ = [
     "Vgg1D",
+    "SimpleFullyConnected",
     "ResNet1D",
 ]
---
+++ sparkle_stats/models/simple_fully_connected.py
@@ -0,0 +1,21 @@
+from torch import nn
+
+from sparkle_stats.sample_parameters import PARAMETER_COUNT
+
+
+class SimpleFullyConnected(nn.Module):
+    def __init__(self, output_classes=PARAMETER_COUNT, input_size=4000):
+        super().__init__()
+
+        self.network = nn.Sequential(
+            nn.Linear(input_size, 2048),
+            nn.ReLU(inplace=True),
+            nn.Linear(2048, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, output_classes),
+        )
+
+    def forward(self, raw):
+        return self.network(raw)
