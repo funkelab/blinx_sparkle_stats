8fb168da7c4f72f79aa5bf2c3c7731b753b0119e
Yogesh Thambidurai <thambiduraiy@hhmi.org>
2024-07-08 13:47:35 -0400

    fix loader code not matching new normalization factors

--- sparkle_stats/training/loaders.py
+++ sparkle_stats/training/loaders.py
@@ -5,24 +5,20 @@ def load_path(
     dataset_type,
     path,
     device,
-    trace_mean=None,
-    parameter_means=None,
-    parameter_std_devs=None,
+    normalization_data_dir=None,
     load_all=True,
     batch_size=100,
 ):
     ds = dataset_type(
         path,
-        trace_mean=trace_mean,
-        parameter_means=parameter_means,
-        parameter_std_devs=parameter_std_devs,
+        normalization_data_dir=normalization_data_dir,
         load_all=load_all,
     )
     ds.traces = ds.traces.to(device)
+    ds.traces_mean = ds.traces_mean.to(device)
     ds.parameters = ds.parameters.to(device)
-    ds.parameter_means = ds.parameter_means.to(device)
-    ds.parameter_std_devs = ds.parameter_std_devs.to(device)
-    ds.trace_mean = ds.trace_mean.to(device)
+    ds.parameters_means = ds.parameters_means.to(device)
+    ds.parameters_std_devs = ds.parameters_std_devs.to(device)
 
     loader = DataLoader(ds, batch_size=batch_size, shuffle=True)
     return ds, loader
