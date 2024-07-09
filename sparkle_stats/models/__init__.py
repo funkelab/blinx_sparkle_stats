6a21773fb8c739a2fde05fa955e2f9f759fa6418
Yogesh Thambidurai <thambiduraiy@hhmi.org>
2024-07-09 14:01:58 -0400

    add simple vgg

--- sparkle_stats/models/__init__.py
+++ sparkle_stats/models/__init__.py
@@ -1,9 +1,11 @@
 from .resnet1d import ResNet1D
 from .simple_fully_connected import SimpleFullyConnected
 from .vgg1d import Vgg1D
+from .vgg_simple import SimpleVgg
 
 __all__ = [
     "Vgg1D",
     "SimpleFullyConnected",
     "ResNet1D",
+    "SimpleVgg",
 ]
