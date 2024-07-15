fabcc56afa80b5b7c11c609de50d210dd100a2f3
Yogesh Thambidurai <thambiduraiy@hhmi.org>
2024-07-15 09:29:38 -0400

    add ponpoff dataset

--- sparkle_stats/zarr_dataset.py
+++ sparkle_stats/zarr_dataset.py
@@ -127,6 +127,23 @@ class ZarrIntensityOnlyDataset(ZarrDataset):
         return 7
 
 
+class PonPoffOnlyDataset(ZarrIntensityOnlyDataset):
+    """
+    Pairs intensity traces with only p_on and p_off
+    """
+
+    def __getitem__(self, item):
+        t, p = super().__getitem__(item)
+        # only show p_on and p_off
+        p = p[:, [5, 6]]
+        print(p.shape)
+        return t, p
+
+    @property
+    def output_classes(self):
+        return 2
+
+
 class ZarrStateOnlyDataset(ZarrDataset):
     def __getitem__(self, item):
         trace, parameters = super().__getitem__(item)
