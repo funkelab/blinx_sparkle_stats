e1c6ab806c07c7ed7b08e7f1ef148e0f8d5860eb
Yogesh Thambidurai <thambiduraiy@hhmi.org>
2024-07-15 16:43:41 -0400

    refactor zarr_dataset to have checks in function and avoid variable reassignment

--- sparkle_stats/datasets/zarr_dataset.py
+++ sparkle_stats/datasets/zarr_dataset.py
@@ -25,18 +25,11 @@ class ZarrDataset(Dataset):
                 - whether to load everything into memory at once
         """
 
-        if not os.path.exists(data_dir):
-            raise FileNotFoundError(f"Directory {data_dir} not found")
-        if not os.path.isdir(data_dir):
-            raise NotADirectoryError(f"Found file at {data_dir} expected directory")
+        _directory_exists(data_dir)
 
         if normalization_data_dir is None:
             normalization_data_dir = data_dir
-
-        if not os.path.exists(normalization_data_dir):
-            raise FileNotFoundError(f"Directory {data_dir} not found")
-        if not os.path.isdir(normalization_data_dir):
-            raise NotADirectoryError(f"Found file at {data_dir} expected directory")
+        _directory_exists(normalization_data_dir)
 
         traces_path, _, _, parameters_path, _, _, _ = get_file_paths_from_base(data_dir)
 
@@ -50,50 +43,23 @@ class ZarrDataset(Dataset):
             _,
         ) = get_file_paths_from_base(normalization_data_dir)
 
-        if not os.path.exists(traces_path):
-            raise FileNotFoundError(f"Can't find traces at {traces_path}")
-        if not os.path.exists(traces_max_path):
-            raise FileNotFoundError(f"Can't find traces max at {traces_max_path}")
-        if not os.path.exists(traces_min_path):
-            raise FileNotFoundError(f"Can't find traces min at {traces_min_path}")
-        if not os.path.exists(parameters_path):
-            raise FileNotFoundError(f"Can't find parameters at {parameters_path}")
-        if not os.path.exists(parameters_max_path):
-            raise FileNotFoundError(
-                f"Can't find parameters max at {parameters_max_path}"
-            )
-        if not os.path.exists(parameters_min_path):
-            raise FileNotFoundError(
-                f"Can't find parameters min at {parameters_min_path}"
-            )
+        _path_exists(traces_path, "traces")
+        _path_exists(traces_min_path, "traces min")
+        _path_exists(traces_max_path, "traces max")
+        _path_exists(parameters_path, "parameters")
+        _path_exists(parameters_min_path, "parameters min")
+        _path_exists(parameters_max_path, "parameters max")
 
-        self.traces = zarr.open(traces_path, mode="r")
-        self.traces_max = np.load(traces_max_path)
-        self.traces_min = np.load(traces_min_path)
-        self.parameters = zarr.open(parameters_path, mode="r")
-        self.parameters_max = np.load(parameters_max_path)
-        self.parameters_min = np.load(parameters_min_path)
+        traces = zarr.open(traces_path, mode="r")
+        parameters = zarr.open(parameters_path, mode="r")
 
-        self.traces_max = torch.from_numpy(np.array(self.traces_max).astype(np.float32))
-        self.traces_min = torch.from_numpy(np.array(self.traces_min).astype(np.float32))
-        self.parameters_max = torch.from_numpy(
-            np.array(self.parameters_max).astype(np.float32)
-        )
-        self.parameters_min = torch.from_numpy(
-            np.array(self.parameters_min).astype(np.float32)
-        )
-
-        # torch data loader changes the data if there is an attribute named "load_all" set to true on this class.
-        # obfuscating the name to __load_all fixes the problem?
-        # can't reproduce
-        self.__load_all = load_all
-        if self.__load_all:
-            # load into memory
-            self.traces = np.array(self.traces).astype(np.float32)
-            self.parameters = np.array(self.parameters).astype(np.float32)
-            # convert to torch
-            self.traces = torch.from_numpy(self.traces)
-            self.parameters = torch.from_numpy(self.parameters)
+        self.load_all = load_all
+        if self.load_all:
+            self.traces = torch.from_numpy(np.array(traces).astype(np.float32))
+            self.parameters = torch.from_numpy(np.array(parameters).astype(np.float32))
+        else:
+            self.traces = traces
+            self.parameters = parameters
 
         self.trace_count = self.traces.shape[0]
         self.trace_length = self.traces.shape[1]
@@ -104,27 +70,40 @@ class ZarrDataset(Dataset):
                 f"Found {parameters_length} parameters but {self.trace_count} traces"
             )
 
+        self.traces_max = torch.from_numpy(np.load(traces_max_path).astype(np.float32))
+        self.traces_min = torch.from_numpy(np.load(traces_min_path).astype(np.float32))
+        self.parameters_max = torch.from_numpy(
+            np.load(parameters_max_path).astype(np.float32)
+        )
+        self.parameters_min = torch.from_numpy(
+            np.load(parameters_min_path).astype(np.float32)
+        )
+
     def __len__(self):
         return self.trace_count
 
     def __getitem__(self, item):
         # is currently NxTxC
         # reshape into CxT
-        trace = self.traces[item, :, :].T
-        parameters = self.parameters[item, :].reshape(-1)
+        raw_trace = self.traces[item, :, :].T
+        raw_parameters = self.parameters[item, :].reshape(-1)
 
-        if not self.__load_all:
-            trace = torch.from_numpy(trace.astype(np.float32))
-            parameters = torch.from_numpy(parameters.astype(np.float32))
+        if not self.load_all:
+            tensor_trace = torch.from_numpy(raw_trace.astype(np.float32))
+            tensor_parameters = torch.from_numpy(raw_parameters.astype(np.float32))
+        else:
+            # will already be tensors from the constructor
+            # clone to avoid overwriting the backing data
+            tensor_trace = raw_trace.clone()
+            tensor_parameters = raw_parameters.clone()
 
-        # have to clone the trace so that the first dimension isn't overwritten
-        # otherwise results in bug where get_item changes the backing array
-        trace = trace.clone()
-        trace[0] = (trace[0] - self.traces_min) / (
+        trace_intensity = (tensor_trace[0] - self.traces_min) / (
             self.traces_max - self.traces_min
         ) * 2 - 1
+        trace_z = tensor_trace[1]
+        trace = torch.vstack((trace_intensity, trace_z))
 
-        parameters = (parameters - self.parameters_min) / (
+        parameters = (tensor_parameters - self.parameters_min) / (
             self.parameters_max - self.parameters_min
         ) * 2 - 1
 
@@ -133,3 +112,15 @@ class ZarrDataset(Dataset):
     @property
     def output_classes(self):
         return 7
+
+
+def _directory_exists(path):
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Directory {path} not found")
+    if not os.path.isdir(path):
+        raise NotADirectoryError(f"Found file at {path} expected directory")
+
+
+def _path_exists(path, name):
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Can't find {name} at {path}")
