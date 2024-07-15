3450678dd03a93ce9fd0ecf20b558585693b93e1
Yogesh Thambidurai <thambiduraiy@hhmi.org>
2024-07-15 09:30:17 -0400

    fix dataloaders trying to move zarrs

--- sparkle_stats/training/loaders.py
+++ sparkle_stats/training/loaders.py
@@ -14,11 +14,12 @@ def load_path(
         normalization_data_dir=normalization_data_dir,
         load_all=load_all,
     )
-    ds.traces = ds.traces.to(device)
-    ds.traces_mean = ds.traces_mean.to(device)
-    ds.parameters = ds.parameters.to(device)
-    ds.parameters_means = ds.parameters_means.to(device)
-    ds.parameters_std_devs = ds.parameters_std_devs.to(device)
+    if load_all:
+        ds.traces = ds.traces.to(device)
+        ds.traces_mean = ds.traces_mean.to(device)
+        ds.parameters = ds.parameters.to(device)
+        ds.parameters_means = ds.parameters_means.to(device)
+        ds.parameters_std_devs = ds.parameters_std_devs.to(device)
 
     loader = DataLoader(ds, batch_size=batch_size, shuffle=True)
     return ds, loader
