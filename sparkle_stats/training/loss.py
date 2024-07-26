8fd02165e01a453a86f00cb185cf97b4111b227f
Yogesh Thambidurai <thambiduraiy@hhmi.org>
2024-07-26 10:13:44 -0400

    new loss function

---
+++ sparkle_stats/training/loss.py
@@ -0,0 +1,36 @@
+import torch
+
+
+def likelihood_loss(labels, predictions):
+    # labels: B, 2*classes
+    # predictions: B, classes
+    assert (
+        len(predictions.shape) == 2
+    ), f"Expected predictions to be of shape (B, 2 * classes), found shape {predictions.shape}"
+    assert (
+        predictions.shape[1] % 2 == 0
+    ), f"Expected predictions to be of shape (B, (2 * classes)), found shape {predictions.shape}"
+
+    num_classes = predictions.shape[1] // 2
+    assert (
+        len(labels.shape) == 2
+    ), f"Expected labels to be of shape (B, {num_classes}), found shape {labels.shape}"
+    assert (
+        labels.shape[1] == num_classes
+    ), f"Expected labels to be of shape (B, {num_classes}), found shape {labels.shape}"
+
+    # should turn
+    #  [1 2 3 4 5 6]
+    #
+    # into
+    # [[1, 2]
+    #  [3, 4]
+    #  [5, 6]]
+
+    predictions = predictions.reshape(-1, num_classes, 2)
+
+    means = predictions[:, :, 0]
+    variances = predictions[:, :, 1]
+    dist = torch.distributions.Normal(means, variances)
+    probs = dist.log_prob(labels)
+    return probs.sum(axis=1)
---
+++ tests/test_likelihood_loss.py
@@ -0,0 +1,26 @@
+import jax
+import numpy as np
+import torch
+from sparkle_stats.training.loss import likelihood_loss
+
+
+def test_likelihood_loss():
+    labels = torch.arange(5 * 7).reshape(5, 7)
+    predictions = torch.arange(5 * 7 * 2).reshape(5, 7 * 2)
+
+    loss = likelihood_loss(labels, predictions)
+    assert loss.shape == (5,)
+
+
+def test_likelihood_loss_against_jax():
+    labels = torch.tensor([[0.0]])
+    predictions = torch.tensor([[0.0, 1.0]])
+
+    loss = likelihood_loss(labels, predictions)
+    assert loss.shape == (1,)
+
+    jax_loss = jax.scipy.stats.norm.logpdf(0.0, 0.0, 1.0)
+
+    loss = loss.detach().numpy()
+    jax_loss = np.array(jax_loss)
+    assert np.isclose(loss, jax_loss).all()
